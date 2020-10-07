a3b3fea177d31aaa413e235b33d78536d3774384
amissarova <alsu.missarova.32@gmail.com>
2020-10-07 18:42:45 +0100

    more mem:

--- generateData/markersAtlasAnno/sub_rmd.sh
+++ generateData/markersAtlasAnno/sub_rmd.sh
@@ -9,15 +9,15 @@ script_name=markers_anno
 
 #CHOOSE PARAMETERS
 #RAM in megabytes
-memory=75000
+memory=200000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=5
+nproc=1
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/markersAtlasAnno/run_rmd.R
 
 bsub -q research-rh74 -e ${err_folder}/${script_name} \
 -o ${out_folder}/${script_name} \
--M $memory -R $r_command -n $nproc -J ${script_name} \
+-M $memory -R $r_command -n $nproc -P bigmem -J ${script_name} \
 "singularity exec $smg Rscript $script"
