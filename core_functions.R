0ad8b550ac94d7454ecbe6051960c0a168f178e5
amissarova <alsu.missarova.32@gmail.com>
2020-11-06 19:19:35 +0000

    kidney run PC coords

--- core_functions.R
+++ core_functions.R
@@ -203,8 +203,12 @@ get_suitable4seq_genes = function(dir = "local", system , mean.thresh , max.thre
     root.dir = "/Users/alsu/Develop/geneBasis/"
   }
   
+  
   if (system == "mouse_E8.5") {
     stat = read.table(paste0(root.dir , "data/expression_stat_perGene/stat__mouse_E8_5.tab"), header = T, sep = "\t")
+  }
+  else if (system == "spleen") {
+    stat = read.table(paste0(root.dir , "data/expression_stat_perGene/stat__spleen.tab"), header = T, sep = "\t")
   }
   else if (system == "spleen") {
     stat = read.table(paste0(root.dir , "data/expression_stat_perGene/stat__spleen.tab"), header = T, sep = "\t")
