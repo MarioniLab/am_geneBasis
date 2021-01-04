62e9a102157ea588b21245914835004413c7ae2b
amissarova <alsu.missarova.32@gmail.com>
2021-01-04 16:27:26 +0000

    get loadings

--- functions/main_functions.R
+++ functions/main_functions.R
@@ -17,15 +17,17 @@ get_rid_of_highly_expressed_genes = function(sce , method = "absolute" , mean.th
   }
 }
 
-detect_rare_celltypes = function(sce , nCells.thresh = 10 , FDR.thresh = 0.01){
+detect_rare_celltypes = function(sce , nCells.thresh = 10 , FDR.thresh = 0.05){
   stat.celltype = table(sce$celltype)
   rare_celltypes = stat.celltype[stat.celltype < nCells.thresh]
+  rare_celltypes = as.data.frame(rare_celltypes)
+  colnames(rare_celltypes) = c("celltype" , "n.cells")
   
-  markers.rare_celltypes = lapply(names( rare_celltypes ) , function(celltype){
+  markers.rare_celltypes = lapply(as.character( rare_celltypes$celltype ), function(celltype){
     current.sce = sce
     current.sce$celltype.bool = current.sce$celltype == celltype
     if (stat.celltype[celltype] > 1){
-      markers <- scran::findMarkers(current.sce , groups=current.sce$celltype.bool, direction = "up", test = "binom", assay.type = "logcounts")
+      markers <- scran::findMarkers(current.sce , groups=current.sce$celltype.bool, direction = "up", test = "t", assay.type = "logcounts")
       markers = as.data.frame( markers$`TRUE` )
       markers = markers[markers$FDR < FDR.thresh , ]
       if (nrow(markers) > 0){
@@ -42,7 +44,7 @@ detect_rare_celltypes = function(sce , nCells.thresh = 10 , FDR.thresh = 0.01){
 
 get_rid_of_rare_celltypes = function(sce , nCells.thresh = 10 , FDR.thresh = 0.01){
   rare_celltypes.stat = detect_rare_celltypes(sce , nCells.thresh , FDR.thresh)
-  sce.filtered = sce[, !sce$celltype %in% names( rare_celltypes.stat$celltypes )]
+  sce.filtered = sce[, !sce$celltype %in% unique( rare_celltypes.stat$celltypes$celltype )]
   return(sce.filtered)
 }
 
@@ -135,25 +137,45 @@ get_loadings = function(sce , genes , type){
   return(out)
 }
 
-get_stat_one_pc = function(loadings , pcs , nPC){
-  pnorm.left = pnorm(loadings , mean = median(loadings), sd = mad(loadings), lower.tail = T)
-  pnorm.left = p.adjust(pnorm.left, method = "fdr")
+get_gene_ranking = function(loadings.stat , meta , FDR.thresh = 0.05){
+  loadings = loadings.stat$loadings
+  pcs = loadings.stat$pc
+  pcs = as.data.frame( pcs )
+  colnames(pcs) = paste0("PC_" , c(1:ncol(pcs)))
+  cols.pc = colnames(pcs)
+  pcs = rownames_to_column(pcs , var = "cell")
+  pcs = merge(pcs , meta , all.y = F , all.x = T)
   
-  pnorm.right = pnorm(loadings , mean = median(loadings), sd = mad(loadings), lower.tail = F)
-  pnorm.right = p.adjust(pnorm.right, method = "fdr")
-  
-  out.max = data.frame(nPC = nPC , sign = "pos" , gene = names(which.max(loadings)) , loading = max(loadings) , 
-                       FDR = pnorm.right[names(pnorm.right) == names(which.max(loadings))])
-  out.min = data.frame(nPC = nPC , sign = "neg" , gene = names(which.min(loadings)) , loading = min(loadings), 
-                       FDR =  pnorm.left[names(pnorm.left) == names(which.min(pnorm.left))])
-  out = rbind(out.max , out.min)
-  
-  # add anova
-  current.anova = aov(pcs ~ pcs$sample + pcs$celltype)
-  current.summary = summary(current.anova)[[1]]
-  out = data.frame(pc = current.component , sum_sq.sample = current.summary$`Sum Sq`[1] , sum_sq.celltype = current.summary$`Sum Sq`[2] , 
-                   p.sample = current.summary$`Pr(>F)`[1] , p.celltype = current.summary$`Pr(>F)`[2])
-  
+  stat = lapply(1:ncol(loadings), function(nPC){
+    current.loadings = loadings[, nPC]
+    current.pcs = pcs[, paste0( "PC_" , nPC )]
+    current.pcs = data.frame(cell = pcs$cell , pc = current.pcs )
+    current.pcs = merge(current.pcs , meta , all.y = T , all.x = F)
+    
+    pnorm.left = pnorm(current.loadings , mean = median(current.loadings), sd = mad(current.loadings), lower.tail = T)
+    pnorm.left = p.adjust(pnorm.left, method = "fdr")
+    
+    pnorm.right = pnorm(current.loadings , mean = median(current.loadings), sd = mad(current.loadings), lower.tail = F)
+    pnorm.right = p.adjust(pnorm.right, method = "fdr")
+    
+    out.max = data.frame(nPC = nPC , sign = "pos" , gene = names(which.max(current.loadings)) , loading = max(current.loadings) , 
+                         FDR = pnorm.right[names(pnorm.right) == names(which.max(current.loadings))])
+    out.min = data.frame(nPC = nPC , sign = "neg" , gene = names(which.min(current.loadings)) , loading = min(current.loadings), 
+                         FDR =  pnorm.left[names(pnorm.left) == names(which.min(current.loadings))])
+    out = rbind(out.max , out.min)
+    
+    # add anova
+    current.anova = aov(current.pcs$pc ~ current.pcs$sample + current.pcs$celltype)
+    current.summary = summary(current.anova)[[1]]
+    out.pc = data.frame(nPC = nPC , sum_sq.sample = current.summary$`Sum Sq`[1] , sum_sq.celltype = current.summary$`Sum Sq`[2] , 
+                        p.sample = current.summary$`Pr(>F)`[1] , p.celltype = current.summary$`Pr(>F)`[2])
+    out = merge(out , out.pc , all.x = T)
+    return(out)
+  })
+  stat = do.call(rbind, stat)
+  #stat$gene = as.factor(stat$gene)
+  genes_ranked = unique( stat$gene[stat$FDR < FDR.thresh & stat$p.celltype < FDR.thresh] )
+  out = list(stat = stat , genes = genes_ranked)
   return(out)
 }
 
@@ -181,7 +203,6 @@ get_fraction_mapped_correctly = function(mapping){
 
 
 get_graph = function(genes , sce){
-  require(igraph)
   current.sce = sce[rownames(sce) %in% genes , ]
   current.counts = cosineNorm(logcounts(current.sce))
   batchFactor = factor(c(as.character(current.sce$sample)))
@@ -191,6 +212,23 @@ get_graph = function(genes , sce){
   current.graph <- set.vertex.attribute(current.graph, "name", value = rownames(mbpca))
   return(current.graph)
 }
+
+
+get_fraction_shared_neighbors = function(graph_all , graph_subset ){
+  adjacency_list_all = adjacent_vertices(graph_all, V(graph_all), mode = "all")
+  adjacency_list_subset = adjacent_vertices(graph_subset, V(graph_subset), mode = "all")
+  N = length(adjacency_list_all)
+  vertices = names(adjacency_list_all)
+  frac_shared = lapply(vertices, function(v){
+    idx_all = which(names(adjacency_list_all ) == v)
+    idx_subset = which(names(adjacency_list_subset ) == v)
+    out = data.frame(cell = v , frac.shared = length(intersect(adjacency_list_all[[idx_all]] , adjacency_list_subset[[idx_subset]]) ) / length(adjacency_list_all[[idx_all]]))
+    return(out)
+  })
+  frac_shared = do.call(rbind, frac_shared)
+  return(frac_shared)
+}
+
 
 getCorrTranscriptome = function(mapping , genes){
   cols.cells = paste0("V" , c(1:5))
@@ -213,3 +251,72 @@ getCorrTranscriptome = function(mapping , genes){
   stat$n.genes = mapping$n.genes[1]
   return(stat)
 }
+
+
+getHeatMap_mapping = function(mapping){
+  tab = table(mapping$celltype , mapping$celltype.mapped) 
+  tab = sweep(tab, 1, rowSums(tab), "/")
+  tab = as.data.frame( tab)
+  colnames(tab) = c("celltype.actual", "celltype.mapped", "n")
+  tab$celltype.actual = factor(tab$celltype.actual , levels = unique(sce$celltype))
+  tab$celltype.mapped = factor(tab$celltype.mapped , levels = c(unique(sce$celltype),"Unmapped"))
+  tab = tab[!is.na(tab$celltype.actual) , ]
+  p <- ggplot(tab, aes(x = celltype.actual , y = celltype.mapped, fill = n)) + 
+    geom_tile() + scale_fill_viridis(discrete = F) + 
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) + 
+    ggtitle(paste0("n.genes = " , mapping$n.genes[1]))
+  return(p)
+}
+
+add_markers_posthoc = function(sce , genes , celltype.1 , celltype.2 = NULL ){
+  if (!is.null(celltype.2)){
+    current.sce = sce[, sce$celltype %in% union(celltype.1 , celltype.2)]
+  } 
+  else {
+    current.sce = sce
+  }
+  current.markers = findMarkers(current.sce, current.sce$celltype == celltype.1 , test = "binom", assay.type = "logcounts", direction = "up" , pval.type = "any" )
+  current.markers = as.data.frame(current.markers[[2]])
+  current.markers = current.markers[current.markers$logFC.FALSE > 0 & !rownames(current.markers) %in% genes , ]
+  current.markers = current.markers[order(current.markers$FDR , decreasing = F),]
+  return(current.markers)
+}
+
+
+get_mapping_robustness = function(genes, sce){
+  mapping_all = get_mapping(genes , sce)
+  stat_all = get_fraction_mapped_correctly(mapping_all)
+  colnames(stat_all) = c("celltype" , "frac_correctly_mapped.all" )
+  stat = lapply(genes , function(gene){
+    genes.2keep = setdiff(genes, gene)
+    current.mapping = get_mapping(genes.2keep , sce)
+    current.stat = get_fraction_mapped_correctly(current.mapping)
+    current.stat = merge(current.stat , stat_all , by = "celltype")
+    current.stat$frac_correctly_mapped.ratio = current.stat$frac_correctly_mapped / current.stat$frac_correctly_mapped.all
+    current.stat = current.stat[, c("celltype" , "frac_correctly_mapped.ratio")]
+    current.stat$gene = gene
+    return(current.stat)
+  })
+  stat = do.call(rbind , stat)
+  return(stat)
+}
+
+
+
+
+getDistrPlot = function(sce , assay.type , gene){
+  if (assay.type == "counts"){
+    current.counts = as.matrix(counts(sce[gene , ]))
+  }
+  else if (assay.type == "logcounts"){
+    current.counts = as.matrix(logcounts(sce[gene , ]))
+  }
+  current.counts = data.frame(cell = sce$cell , celltype = sce$celltype , counts = as.numeric( current.counts) )
+  p <- ggplot(current.counts , aes(x = celltype , y = counts , fill = celltype)) + 
+    geom_violin() + geom_boxplot() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) + 
+    theme(legend.position = "none") + 
+    ggtitle(gene)
+  return(p)
+}
+
