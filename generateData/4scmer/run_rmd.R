e19d3d7474744c685ccd2790f91e3d2016c0eeeb
amissarova <alsu.missarova.32@gmail.com>
2021-02-09 13:46:08 +0000

    gen data 4 scmer

---
+++ generateData/4scmer/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/4scmer/gen_data_4scmer.Rmd")
---
+++ generateData/4scmer/sub_rmd.sh
@@ -0,0 +1,23 @@
+#INITIALISE FOLDERS
+my_folder=/nfs/research1/marioni/alsu
+out_folder=${my_folder}/clust_out/geneBasis
+err_folder=${my_folder}/clust_err/geneBasis
+
+#SELECT SCRIPT
+#If you change this, you MUST update the wrapper's grep
+script_name=gen_4scmer
+
+#CHOOSE PARAMETERS
+#RAM in megabytes
+memory=300000
+r_command="rusage[mem=${memory}]"
+#num_processors
+nproc=1
+
+smg=/nfs/research1/marioni/alsu/singularity/R1.simg
+script=/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/4scmer/run_rmd.R
+
+bsub -q research-rh74 -e ${err_folder}/${script_name} \
+-o ${out_folder}/${script_name} \
+-M $memory -R $r_command -n $nproc -P bigmem -J ${script_name} \
+"singularity exec $smg Rscript $script"
