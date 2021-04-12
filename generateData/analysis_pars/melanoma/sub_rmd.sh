66b8255681420982201ca36c6fb41939ccd073e8
amissarova <alsu.missarova.32@gmail.com>
2021-04-12 19:41:33 +0100

    analysis melanoma

--- generateData/analysis_pars/melanoma/sub_rmd.sh
+++ generateData/analysis_pars/melanoma/sub_rmd.sh
@@ -5,19 +5,19 @@ err_folder=${my_folder}/clust_err/geneBasis
 
 #SELECT SCRIPT
 #If you change this, you MUST update the wrapper's grep
-script_name=genes_pan
+script_name=anl_mel
 
 #CHOOSE PARAMETERS
 #RAM in megabytes
-memory=200000
+memory=150000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=8
+nproc=6
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/analysis_pars/melanoma/run_rmd.R
 
 bsub -q research-rh74 -e ${err_folder}/${script_name} \
 -o ${out_folder}/${script_name} \
--M $memory -R $r_command -n $nproc -P bigmem -J ${script_name} \
+-M $memory -R $r_command -n $nproc -J ${script_name} \
 "singularity exec $smg Rscript $script"
