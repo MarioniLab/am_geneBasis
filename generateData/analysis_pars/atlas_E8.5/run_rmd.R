ad4fe617875ae7ca6cdee01b9f15945fa025d586
amissarova <alsu.missarova.32@gmail.com>
2021-04-12 21:12:01 +0100

    add for others

---
+++ generateData/analysis_pars/atlas_E8.5/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/analysis_pars/atlas_E8.5/analysis_atlas_E8.5.Rmd")
---
+++ generateData/analysis_pars/pancreas/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/analysis_pars/pancreas/analysis_pancreas.Rmd")
