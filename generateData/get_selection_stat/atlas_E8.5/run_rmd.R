44982e16bdbe0d9963259f015fceca0fc83f778e
amissarova <alsu.missarova.32@gmail.com>
2021-04-15 17:32:02 +0100

    codes to generate stat re selection

---
+++ generateData/get_selection_stat/atlas_E8.5/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/get_selection_stat/atlas_E8.5/get_selection_stat.Rmd")
---
+++ generateData/get_selection_stat/pancreas/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/get_selection_stat/pancreas/get_selection_stat.Rmd")
