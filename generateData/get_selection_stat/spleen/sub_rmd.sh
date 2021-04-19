9b34a3353a88c01b66556d80a1657741c35ccd4c
amissarova <alsu.missarova.32@gmail.com>
2021-04-19 19:43:01 +0100

    spleen

--- generateData/get_selection_stat/spleen/sub_rmd.sh
+++ generateData/get_selection_stat/spleen/sub_rmd.sh
@@ -11,7 +11,7 @@ script_name=stat_spl
 memory=550000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=10
+nproc=8
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/get_selection_stat/spleen/run_rmd.R
