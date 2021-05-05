511f81b733d048fbd50150ad97efa0d0538497e0
amissarova <alsu.missarova.32@gmail.com>
2021-05-05 10:41:44 +0100

    fix

--- generateData/benchmark/gene_score/spleen/sub_rmd.sh
+++ generateData/benchmark/gene_score/spleen/sub_rmd.sh
@@ -8,10 +8,10 @@ err_folder=${my_folder}/clust_err/geneBasis
 script_name=gs_spl
 #CHOOSE PARAMETERS
 #RAM in megabytes
-memory=200000
+memory=300000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=1
+nproc=5
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/benchmark/gene_score/spleen/run_rmd.R
