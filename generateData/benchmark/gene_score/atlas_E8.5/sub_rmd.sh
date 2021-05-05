94597d2d8322fb708ce90e8b4e8ffc3b3fc56f7b
amissarova <alsu.missarova.32@gmail.com>
2021-05-05 09:26:48 +0100

    fix

--- generateData/benchmark/gene_score/atlas_E8.5/sub_rmd.sh
+++ generateData/benchmark/gene_score/atlas_E8.5/sub_rmd.sh
@@ -11,7 +11,7 @@ script_name=gs_emb
 memory=200000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=1
+nproc=5
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/benchmark/gene_score/atlas_E8.5/run_rmd.R
