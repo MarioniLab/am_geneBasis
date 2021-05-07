6f7c88c9d738d0df6c98c3b65504233e0b882e79
amissarova <alsu.missarova.32@gmail.com>
2021-05-07 12:42:10 +0100

    fix

--- generateData/get_selection/pancreas/sub_rmd.sh
+++ generateData/get_selection/pancreas/sub_rmd.sh
@@ -9,10 +9,10 @@ script_name=genes_pan
 
 #CHOOSE PARAMETERS
 #RAM in megabytes
-memory=300000
+memory=500000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=5
+nproc=10
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/get_selection/pancreas/run_rmd.R
