797e23b406730574a095338604c18c722820e71a
amissarova <alsu.missarova.32@gmail.com>
2021-05-09 23:15:05 +0100

    fix

---
+++ generateData/redundancy/atlas_E8.5/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/redundancy/atlas_E8.5/get_redundancy.Rmd")
---
+++ generateData/redundancy/pancreas/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/redundancy/pancreas/get_redundancy.Rmd")
