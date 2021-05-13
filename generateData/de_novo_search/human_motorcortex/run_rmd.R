78d8c8b82e1558df553b9fdd2dc63792eba0164b
amissarova <alsu.missarova.32@gmail.com>
2021-05-13 16:22:47 +0100

    delete old

--- generateData/de_novo_search/human_motorcortex/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/de_novo_search/human_motorcortex/de_novo_search.Rmd")
--- generateData/de_novo_search/pancreas/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/de_novo_search/pancreas/de_novo_search.Rmd")
