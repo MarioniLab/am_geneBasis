70cae7a7ad78ba85f03978dc344d8fccbbe7b50e
amissarova <alsu.missarova.32@gmail.com>
2021-05-13 16:14:30 +0100

    fix

--- generateData/get_PCs/atlas_E8.5/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/get_PCs/atlas_E8.5/get_PCs.Rmd")
--- generateData/get_PCs/spleen/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/get_PCs/spleen/get_PCs.Rmd")
