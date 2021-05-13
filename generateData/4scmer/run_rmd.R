dfd2ed3446fa5106e576ad83c1aa9e7b12932554
amissarova <alsu.missarova.32@gmail.com>
2021-05-13 16:08:54 +0100

    delete old

--- generateData/4scmer/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/4scmer/gen_data_4scmer.Rmd")
--- generateData/analysis_pars/pancreas/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/analysis_pars/pancreas/analysis_pancreas.Rmd")
