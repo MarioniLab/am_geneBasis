575a8d26a702368941bda05ccecb4e0bca9c66ef
amissarova <alsu.missarova.32@gmail.com>
2021-05-13 16:16:24 +0100

    delete old

--- generateData/main/atlas_E8.5/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/main/atlas_E8.5/main_atlas_E8.5.Rmd")
--- generateData/main/kidney/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/main/kidney/main_kidney.Rmd")
