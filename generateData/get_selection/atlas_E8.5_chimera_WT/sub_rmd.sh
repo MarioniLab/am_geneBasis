c51e42106af534d225ab1d9e6a8e046d87426e8a
amissarova <alsu.missarova.32@gmail.com>
2021-05-25 14:05:35 +0100

    update gene search for wt chimera

--- generateData/get_selection/atlas_E8.5_chimera_WT/sub_rmd.sh
+++ generateData/get_selection/atlas_E8.5_chimera_WT/sub_rmd.sh
@@ -9,10 +9,10 @@ script_name=genes_emb_ch_wt
 
 #CHOOSE PARAMETERS
 #RAM in megabytes
-memory=200000
+memory=300000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=1
+nproc=2
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/geneBasis/am_geneBasis/generateData/get_selection/atlas_E8.5_chimera_WT/run_rmd.R
