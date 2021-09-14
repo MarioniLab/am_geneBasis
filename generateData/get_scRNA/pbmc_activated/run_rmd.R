1cc9943b274718d742e244abb67c7c3ee6d8d85d
amissarova <alsu.missarova.32@gmail.com>
2021-09-14 09:32:43 +0100

    pbmc activated sce

---
+++ generateData/get_scRNA/pbmc_activated/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research/marioni/alsu/geneBasis/am_geneBasis/generateData/get_scRNA/pbmc_activated/pbmc_activated.Rmd")
---
+++ generateData/get_scRNA/pbmc_activated/sub_rmd.sh
@@ -0,0 +1,23 @@
+#INITIALISE FOLDERS
+my_folder=/nfs/research1/marioni/alsu
+out_folder=${my_folder}/clust_out/geneBasis
+err_folder=${my_folder}/clust_err/geneBasis
+
+#SELECT SCRIPT
+#If you change this, you MUST update the wrapper's grep
+script_name=sc_pbmc
+
+#CHOOSE PARAMETERS
+#RAM in megabytes
+memory=600000
+r_command="rusage[mem=${memory}]"
+#num_processors
+nproc=7
+
+smg=/hps/software/users/marioni/alsu/singularity/alsu_image.simg
+script=/nfs/research/marioni/alsu/geneBasis/am_geneBasis/generateData/get_scRNA/pbmc_activated/run_rmd.R
+
+bsub -q standard -e ${err_folder}/${script_name} \
+-o ${out_folder}/${script_name} \
+-M $memory -R $r_command -n $nproc -J ${script_name} \
+"singularity exec $smg Rscript $script"
