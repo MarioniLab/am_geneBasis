a455c3f862c6a26666931c760dad89b120c28072
amissarova <alsu.missarova.32@gmail.com>
2021-09-16 19:28:32 +0100

    brain cortex

---
+++ generateData/get_selection/brain_cortex/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research/marioni/alsu/geneBasis/am_geneBasis/generateData/get_selection/brain_cortex/gene_selection.Rmd")
---
+++ generateData/get_selection/brain_cortex/sub_rmd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#INITIALISE FOLDERS
+my_folder=/nfs/research/marioni/alsu
+out_folder=${my_folder}/clust_out/geneBasis
+err_folder=${my_folder}/clust_err/geneBasis
+
+#SELECT SCRIPT
+#If you change this, you MUST update the wrapper's grep
+script_name=genes_cortex
+
+#CHOOSE PARAMETERS
+#RAM in megabytes
+memory=200000
+r_command="rusage[mem=${memory}]"
+#num_processors
+nproc=1
+
+smg=/hps/software/users/marioni/alsu/singularity/alsu_image.simg
+script=/nfs/research/marioni/alsu/geneBasis/am_geneBasis/generateData/get_selection/brain_cortex/run_rmd.R
+
+bsub -q bigmem -e ${err_folder}/${script_name} \
+-o ${out_folder}/${script_name} \
+-M $memory -R $r_command -n $nproc -J ${script_name} \
+"singularity exec $smg Rscript $script"
